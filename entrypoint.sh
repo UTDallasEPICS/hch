66a979329fdf3bb3776d93fe1dcab116c6ad9f4c
TusharW4ni <reachtusharwani@gmail.com>
2026-04-26 20:48:50 -0500

    fix(stage): compile prisma seed to js to avoid tsx OOM

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Apply migrations and initialize migrations if it does not exist
 pnpm prisma generate
 pnpm prisma migrate deploy
-pnpm prisma db seed
+node prisma/seed.mjs
 
 # Run the CMD command from the dockerfile
 exec "$@"
